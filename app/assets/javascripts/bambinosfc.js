de044dd919e9c3e89484c590dcbf29732df3faaa
Tony Marklove <tony@new-bamboo.co.uk>
2013-05-03 16:58:40 +0100

    Working with some kind of trails

--- app/assets/javascripts/bambinosfc.js
+++ app/assets/javascripts/bambinosfc.js
@@ -20,11 +20,10 @@ jQuery(function($) {
 
   // var data = [ { x: 0, y: 100 }, { x: 100, y: 300 }, { x: 100, y: 500 } ];
 
-  var pointColour = d3.scale.category20b();
+  var pointColour = d3.scale.category10();
   var index = 0;
 
   updateScales(teamPointsByMatchday.length, getHighestPointsInLeague(teamPointsByMatchday));
-  console.log(teamPointsByMatchday.length);
 
   var chart = d3.select('svg')
     .append('g')
@@ -34,16 +33,39 @@ jQuery(function($) {
 
   $(document).click(function() {
     index = (index + 1) % teamPointsByMatchday.length;
-    console.log(index);
     onData(teamPointsByMatchday[index]);
+    onLineData(createLineData(teamPointsByMatchday.slice(0, index+1)));
   });
 
+
+  lineData = createLineData(teamPointsByMatchday);
+
+  var line = d3.svg.line()
+    .interpolate("linear")
+    .x(function(d) { return xScale(d.match) + xScale.rangeBand()/2; })
+    .y(function(d) { return yScale(d.points); });
+
+  function onLineData(data) {
+    var team = chart.selectAll(".line")
+      .data(data, function(d) { return d.name; })
+      .enter().append("g")
+        .attr("class", "line");
+
+    team.append("path")
+      .attr("class", "line")
+      .attr("d", function(d) { return line(d.values); })
+      .style("stroke", function(d, i) { return pointColour(i); })
+      .style("stroke-width", "4px")
+      .style("fill", "transparent");
+
+    d3.selectAll("path").data(data, function(d) { return d.name; }).transition().ease("bounce").duration(1000).attr("d", function(d) { return line(d.values); });
+  }
 
   /*
     Enter
   */
   function onData(data) {
-    function teamTransform(d) { return translateString(xScale(index+1), yScale(d.points)); }
+    function teamTransform(d) { return translateString(xScale(index) + xScale.rangeBand()/2, yScale(d.points)); }
 
     var g = chart.selectAll('g.team')
       .data(data, function(d) { return d.name; })
@@ -54,13 +76,19 @@ jQuery(function($) {
 
     g.append('circle')
       .attr('fill', function(d, i) { return pointColour(i); })
+      .attr('opacity', 0.7)
       .attr('r', 30);
 
     g.append('text')
       .text(function(d) { return d.name; })
-      .attr('x', function(d) { return - this.getBBox().width / 4; })
+      .attr('x', function(d) { return - this.getComputedTextLength() / 4; })
       .attr('y', function(d) { return this.getBBox().height / 4; })
-      .style({ 'font-family': 'Helvetica', 'fill': '#fff', 'font-size': '10px' });
+      .style({
+        'font-family': 'Helvetica',
+        'font-size': '10px',
+        'font-weight': 'bold'
+      })
+      .style('fill', function(d, i) { return Colors.complement(pointColour(i)); });
 
     chart
       .append('g')
@@ -87,14 +115,13 @@ jQuery(function($) {
 
   onData(teamPointsByMatchday[index]);
 
-
   function getHighestPointsInLeague(data) {
-    return d3.max(data.pop(), function(d) { return d.points; });
+    return d3.max(data[data.length-1], function(d) { return d.points; });
   }
 
   function updateScales(xMax, yMax) {
     xScale = d3.scale.ordinal()
-              .domain(range(1, xMax))
+              .domain(range(0, xMax-1))
               .rangeBands([0, canvasWidth]);
 
     yScale = d3.scale.linear()
@@ -124,5 +151,25 @@ jQuery(function($) {
       foo.push(i);
     }
     return foo;
+  }
+
+  function createLineData(data) {
+    var lineData = [];
+    var teamNames = data[0].map(function(team) { return team.name; });
+
+    teamNames.forEach(function(teamName) {
+      var values = [];
+
+      data.forEach(function(matchday, index) {
+        matchday.forEach(function(team) {
+          if (team.name != teamName) { return; }
+          values.push({match: index, points: team.points});
+        });
+      });
+
+      lineData.push({name: teamName, values: values});
+    });
+
+    return lineData;
   }
 });

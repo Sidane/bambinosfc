3dc2d539b63bda32882fc450b4ab56fab93eeee0
Damon Davison <damon@allolex.net>
2013-05-03 13:08:57 +0100

    WIP: making X and Y axes

--- app/assets/javascripts/bambinosfc.js
+++ app/assets/javascripts/bambinosfc.js
@@ -1,17 +1,26 @@
 // Bambinos FC
+
+
 jQuery(function($) {
+  var canvasWidth = 1200;
+  var canvasHeight = 800;
+
+  var xScale, yScale;
+
   var svg = d3.select("#bambinos_fc")
       .append("svg")
-      .attr("width", 1200)
-      .attr("height", 800);
+      .attr("width", canvasWidth)
+      .attr("height", canvasHeight);
 
   var data = [ { x: 100, y: 100 }, { x: 100, y: 300 }, { x: 100, y: 500 } ]
 
   var pointColour = d3.scale.category20b();
 
+  // updateScales(teamPointsByMatchday.length, getHighestPointsInLeague(teamPointsByMatchday));
+
   var g = d3.select('svg')
     .selectAll('circle')
-    .data(data)
+    .data(teamPointsByMatchday)
     .enter()
     .append('g')
     .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')' });
@@ -26,5 +35,42 @@ jQuery(function($) {
     .attr('y', function(d) { return this.getBBox().height / 4 })
     .style({ 'font-family': 'Helvetica', 'fill': '#fff', 'font-size': '10px' });
 
+    // d3.select('svg')
+    //   .append('g')
+    //   .attr('id','xAxis')
+    //   .call(makeXAxis)
+
+  function getHighestPointsInLeague(data) {
+    var totals = data[Object.keys(data).sort().pop()];
+
+    var values = [];
+
+    for ( var key in totals ) {
+      values.push(totals[key]);
+    }
+
+    return d3.max(values);
+
+  }
+
+  function updateScales(xMax, yMax) {
+    xScale = d3.scale.linear()
+              .domain([0, xMax])
+              .range(30, canvasWidth - 30);
+
+    yScale = d3.scale.linear()
+              .domain([0, yMax])
+              .range(30, canvasHeight - 30);
+  }
+
+  function makeXAxis(s) {
+    console.log(xScale,s);
+    s.call(d3.svg.axis()
+      .scale(xScale)
+      .orient("bottom"));
+  }
+
 
 })
+
+
